9e60b0e800e2677edf52a10c37e9fce9e91b362a
Karl Rosaen <krosaen@gmail.com>
2019-01-27 15:52:18 -0500

    vec3 and ray beginnings

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,65 @@
-use std::path::Path;
 use std::fs::File;
 use std::io::BufWriter;
+use std::ops;
+use std::path::Path;
 // To use encoder.set()
 use png::HasParameters;
+
+struct Vec3(f64, f64, f64);
+
+impl Vec3 {
+    fn x(&self) -> f64 {
+        self.0
+    }
+    fn y(&self) -> f64 {
+        self.1
+    }
+    fn z(&self) -> f64 {
+        self.2
+    }
+    fn r(&self) -> f64 {
+        self.0
+    }
+    fn g(&self) -> f64 {
+        self.1
+    }
+    fn b(&self) -> f64 {
+        self.2
+    }
+}
+
+impl ops::Add for Vec3 {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Vec3(self.0 + other.0, self.1 + other.1, self.2 + other.2)
+    }
+}
+
+impl ops::Mul<f64> for Vec3 {
+    type Output = Self;
+
+    fn mul(self, other: f64) -> Self {
+        Vec3(self.0 * other, self.1 * other, self.2 * other)
+    }
+}
+
+struct Ray {
+    a: Vec3,
+    b: Vec3,
+}
+
+impl Ray {
+    fn origin(&self) -> &Vec3 {
+        &self.a
+    }
+    fn direction(&self) -> &Vec3 {
+        &self.b
+    }
+    fn point_at_parameter(self, t: f64) -> Vec3 {
+        self.a + (self.b * t)
+    }
+}
 
 fn main() {
     let nx = 200;
@@ -10,12 +67,10 @@ fn main() {
     let mut img_data = Vec::new();
     for j in (0..ny).rev() {
         for i in 0..nx {
-            let r = (i as f64) / (nx as f64);
-            let g = (j as f64) / (ny as f64);
-            let b = 0.2;
-            let ir = (255.99 * r) as u8;
-            let ig = (255.99 * g) as u8;
-            let ib = (255.99 * b) as u8;
+            let color = Vec3((i as f64) / (nx as f64), (j as f64) / (ny as f64), 0.2);
+            let ir = (255.99 * color.r()) as u8;
+            let ig = (255.99 * color.g()) as u8;
+            let ib = (255.99 * color.b()) as u8;
             img_data.push(ir);
             img_data.push(ig);
             img_data.push(ib);
@@ -31,6 +86,6 @@ fn save_png(data: &[u8], name: &str) {
     let mut encoder = png::Encoder::new(w, 200, 100);
     encoder.set(png::ColorType::RGBA).set(png::BitDepth::Eight);
     let mut writer = encoder.write_header().unwrap();
-    writer.write_image_data(data).unwrap();    
+    writer.write_image_data(data).unwrap();
     println!("saved {}", name);
 }
